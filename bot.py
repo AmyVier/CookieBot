1afb8fe444f91eba0eda38843b892dbc053beb45
Amy Vier <amyvier@icloud.com>
2024-01-17 14:36:29 -0800

    update

--- bot.py
+++ bot.py
@@ -10,14 +10,15 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('hello {0.user}'.format(client)):
+        await message.channel.send('hello!')
 
-client.run()
+client.run(TOKEN)
 
